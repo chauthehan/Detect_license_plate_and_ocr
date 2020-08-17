e6936fd767905fd5364ca0025e82a4cad7c9a803
han <chauthehan00@gmail.com>
2020-08-17 16:20:00 +0700

    detect_vehicle

---
+++ predict.py
@@ -0,0 +1,99 @@
+from object_detection.utils import label_map_util
+from object_detection.utils import ops as utils_ops
+from object_detection.utils import visualization_utils as vis_util
+from PIL import Image
+from IPython.display import display
+import tensorflow as tf
+import numpy as np
+import argparse
+import imutils
+import cv2
+import os
+import pathlib
+
+def load_model(model_name):
+    base_url = 'http://download.tensorflow.org/models/object_detection/'
+    model_file = model_name + '.tar.gz'
+    #Download a file from URL if it not already in the cache
+    model_dir = tf.keras.utils.get_file(
+        fname=model_name,
+        origin=base_url + model_file,
+        untar=True)
+    model_dir = pathlib.Path(model_dir)/"saved_model"
+
+    model = tf.saved_model.load(str(model_dir))
+
+    return model
+
+def run_inference_for_single_image(model, image):
+    image = np.asarray(image)
+    #the input needs to be a tensor, convert it using tf.convert_to_tensor'
+    input_tensor = tf.convert_to_tensor(image)
+    input_tensor = input_tensor[tf.newaxis,...]
+
+    model_fn = model.signatures['serving_default']
+    output_dict = model_fn(input_tensor)
+
+    num_detections = int(output_dict.pop('num_detections'))
+    output_dict = {key:value[0, :num_detections].numpy()
+                    for key, value in output_dict.items()}
+    output_dict['num_detections'] = num_detections
+
+    output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
+
+    if 'detection_masks' in output_dict:
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+                output_dict['detection_masks'], output_dict['detection_boxes'],
+                image.shape[0], image.shape[1])      
+        detection_masks_reframed = tf.cast(detection_masks_reframed > 0.5,
+                                        tf.uint8)
+        output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
+
+    return output_dict
+
+def show_inference(model, image_path):
+    image_np = np.array(Image.open(image_path))
+    output_dict = run_inference_for_single_image(model, image_np)
+
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        image_np,
+        output_dict['detection_boxes'],
+        output_dict['detection_classes'],
+        output_dict['detection_scores'],
+        category_index,
+        instance_masks=output_dict.get('detection_masks_reframed', None),
+        use_normalized_coordinates=True,
+        line_thickness=3
+    )
+
+    #display(Image.fromarray(image_np))
+    im = Image.fromarray(image_np)
+    im.show()
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+
+ap.add_argument("-l", "--labels", required=True,
+    help="labels file")
+ap.add_argument("-i", "--image", required=True,
+    help="path to input image")
+ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
+    help="minimum probability used to filter weak detections")
+args = vars(ap.parse_args())
+
+category_index = label_map_util.create_category_index_from_labelmap(args["labels"], use_display_name=True)
+
+model_name = 'ssd_mobilenet_v1_coco_2017_11_17'
+detection_model = load_model(model_name)
+
+print(detection_model.signatures['serving_default'].inputs)
+print(detection_model.signatures['serving_default'].output_dtypes)
+print(detection_model.signatures['serving_default'].output_shapes)
+
+
+show_inference(detection_model, args["image"])
+
+
+
+
